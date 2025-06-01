34aeceb13c0882536ba9a6b9560b7b8daa502d31
OLINYA-PAUL <olinyacpaul7849@gmail.com>
2025-06-01 22:50:05 +0100

    fix user Otp restriction

--- e-shop/apps/auth-service/src/controllers/auth/auth-controller.ts
+++ e-shop/apps/auth-service/src/controllers/auth/auth-controller.ts
@@ -1,40 +1,49 @@
-import { NextFunction, Response, Request } from "express";
+import { NextFunction, Response, Request, response } from "express";
 import { validationError } from "../../../../../packages/error-handler/errorMiddleware";
 import {
   validateRegistrationData,
   checkOtpRestricTion,
+  sendOtp,
+  trackOtpRequest,
 } from "../../utils/auth.helper";
 import prisma from "../../lib/prisma";
 
 interface IAuth {
   email: string;
-  password: string;
+  name: string;
 }
 
-export const authController = async (
+export const registerUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const { email, password } = req.body satisfies IAuth;
+    const { email, name } = req.body satisfies IAuth;
 
     validateRegistrationData(req.body, "user");
 
-    if (!email || !password) {
+    if (!email || !name) {
       throw validationError("Email and password are required");
     }
 
-    const userExsite = await prisma.users.findUnique({
+    const userExist = await prisma.users.findUnique({
       where: { email: email },
     });
-    if (userExsite) {
+    if (userExist) {
       throw validationError("This email is already registered");
     }
 
     // const result  = await prisma.users.create({})
 
     await checkOtpRestricTion(email, next);
+    await trackOtpRequest(email, next);
+    await sendOtp(name, email, "user-activation-mail");
+
+    res.status(201).json({
+      success: true,
+      message: "Otp sent to your email please verify your account",
+    });
   } catch (error) {
     throw validationError(error as Error, req, res);
   }
