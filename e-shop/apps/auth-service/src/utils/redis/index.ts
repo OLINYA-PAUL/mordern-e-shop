58c115596d712065b29a5087faa00b7803afc566
OLINYA-PAUL <olinyacpaul7849@gmail.com>
2025-06-19 23:47:03 +0100

    fix alies and redis error

--- e-shop/apps/auth-service/src/utils/redis/index.ts
+++ e-shop/apps/auth-service/src/utils/redis/index.ts
@@ -1,17 +1,19 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: parseInt(process.env.REDIS_PORT || "6379"),
-  password:
-    process.env.REDIS_PASSWORD ||
-    "AcMCAAIjcDFiZDgzMGZiZDRmZGQ0OGIzODNjZTBlY2NkYjhjOGVjYXAxMA",
-  retryStrategy: (times) => {
-    if (times >= 5) return null;
-    return Math.min(times * 100, 3000);
-  },
-  maxRetriesPerRequest: 5,
-});
+// export const redis = new Redis({
+//   host: process.env.REDIS_HOST || "127.0.0.1",
+//   port: parseInt(process.env.REDIS_PORT || "6379"),
+//   password:
+//     process.env.REDIS_PASSWORD ||
+//     "AcMCAAIjcDFiZDgzMGZiZDRmZGQ0OGIzODNjZTBlY2NkYjhjOGVjYXAxMA",
+//   retryStrategy: (times) => {
+//     if (times >= 5) return null;
+//     return Math.min(times * 100, 3000);
+//   },
+//   maxRetriesPerRequest: 5,
+// });
+
+export const redis = new Redis(process.env.REDIS_URL!);
 
 redis.on("connect", () => {
   console.log("✅ Connected to Redis");
