e5a6124d073e891591da61c9d0fbc2e54c45c9a3
OLINYA-PAUL <olinyacpaul7849@gmail.com>
2025-06-20 18:39:39 +0100

    fix user otp error

--- e-shop/apps/auth-service/src/routes/user.routes.ts
+++ e-shop/apps/auth-service/src/routes/user.routes.ts
@@ -1,8 +1,12 @@
 import express, { Router } from "express";
-import { registerUser } from "../controllers/auth/auth-controller";
+import {
+  registerUser,
+  verifyUserOtp,
+} from "../controllers/auth/auth-controller";
 
 const authrouter: Router = express.Router();
 
 authrouter.post("/user-registration", registerUser);
+authrouter.post("/veriify-otp", verifyUserOtp);
 
 export default authrouter;
