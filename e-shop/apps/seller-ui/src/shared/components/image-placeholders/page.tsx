0fbd943e1da95f9001f02174513cb6182cb8fd61
OLINYA-PAUL <olinyacpaul7849@gmail.com>
2025-08-18 09:49:03 +0100

    create image upload galary fir admin dashboard

--- e-shop/apps/seller-ui/src/shared/components/image-placeholders/page.tsx
+++ e-shop/apps/seller-ui/src/shared/components/image-placeholders/page.tsx
@@ -1,49 +1,118 @@
 'use client';
 
+import { Pencil, WandSparkles, X } from 'lucide-react';
 import React from 'react';
 
 interface ImagePlaceHoldersProps {
-  size?: boolean;
-  small?: boolean | string;
-  onFileChange?: (file: File, index: number) => void;
-  onFileRemove?: (index: number) => void;
+  size?: string;
+  small?: boolean;
+  onFileChange: (file: File, index: number) => void;
+  onFileRemove: (index: number) => void;
   defaultImage?: string | null;
   index?: number | null;
   setOpenImageModel: (open: boolean) => void;
 }
 
 const ImagePlaceHolders = ({
-  size,
   small,
+  size,
   onFileChange,
   onFileRemove,
   defaultImage = null,
   index = null,
   setOpenImageModel,
 }: ImagePlaceHoldersProps) => {
-  const [imagePreview, setImagePreview] = React.useState<string | null>(
-    defaultImage
-  );
+  const [imagePreview, setImagePreview] = React.useState<string | null>(null);
+
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file && onFileChange) {
       setImagePreview(URL.createObjectURL(file));
       onFileChange(file, index ?? 0);
+      onFileRemove(index!);
     }
   };
+
   return (
     <div
-      className={`w-full relative ${
-        small ? 'h-[180px]' : 'h-[450px]'
+      className={`relative ${
+        typeof size === 'string' ? `` : size ? 'h-[180px]' : 'h-[450px]'
       } cursor-pointer p-2 border border-dashed border-slate-600 rounded-md bg-gray-800 backdrop-blur-sm flex flex-col justify-center items-center`}
+      style={
+        typeof size === 'string'
+          ? {
+              width: size.split('x')[0].trim() + 'px',
+              height: size.split('x')[1].trim() + 'px',
+            }
+          : {}
+      }
     >
       <input
         type="file"
-        className="w-full cursor-pointer h-full hidden hover:block "
+        className="hidden"
         accept="image/*"
         id={`image-upload-${index ?? 0}`}
         onChange={handleImageChange}
       />
+
+      {imagePreview ? (
+        <>
+          <button
+            type="button"
+            onClick={() => onFileRemove(index!)}
+            className="absolute top-3 right-3 p-2 rounded bg-red-700 shadow-sm cursor-pointer"
+          >
+            <X size={20} />
+          </button>
+
+          <button
+            type="button"
+            onClick={() => setOpenImageModel(true)}
+            className="absolute top-14 right-3 p-2 rounded bg-blue-700 shadow-sm cursor-pointer"
+          >
+            <WandSparkles size={20} />
+          </button>
+        </>
+      ) : (
+        <label
+          htmlFor={`image-upload-${index}`}
+          className="absolute top-3 right-3 p-2 rounded-full bg-slate-700 shadow-sm cursor-pointer"
+        >
+          <Pencil size={16} />
+        </label>
+      )}
+
+      {imagePreview ? (
+        <>
+          <img
+            src={imagePreview}
+            alt="preview"
+            className="w-full h-full object-cover rounded-md"
+          />
+        </>
+      ) : (
+        <div className="w-full flex items-center flex-col justify-center">
+          <img
+            src={
+              'https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg'
+            }
+            alt="preview"
+            className="w-[70px] h-[70px] opacity-40 mb-5  rounded-md"
+          />
+          <p className="text-xs text-gray-400">
+            {size
+              ? 'Click to upload image'
+              : 'Click the pen icon to Upload image'}{' '}
+            {typeof size === 'string' &&
+              `Size: ${size.split('x')[0].trim()} x ${size
+                .split('x')[1]
+                .trim()}`}
+          </p>
+          <p className="text-gray-500 font-poppins text-sm mt-5">
+            Please choose an image <br /> with the expected ration
+          </p>
+        </div>
+      )}
     </div>
   );
 };
